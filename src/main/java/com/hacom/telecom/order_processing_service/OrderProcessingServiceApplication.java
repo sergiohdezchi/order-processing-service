be011ddcd1033c1577040aa2f3f8c407e28141a8
sergio <sergiohdez.chi@gmail.com>
2025-10-19 18:53:07 -0600

    Project init

---
+++ src/main/java/com/hacom/telecom/order_processing_service/OrderProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hacom.telecom.order_processing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProcessingServiceApplication.class, args);
+	}
+
+}
